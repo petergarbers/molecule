650519377da874b51c1af3dd7e54ace123104768
Peter Garbers <peter@garbers.me>
2016-11-15 19:12:31 -0500

    remove cidr-nrepl dependency

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datomic/datomic-free "0.9.5206" :exclusions [joda-time]]
-                 [cider/cider-nrepl "0.11.0"]]
+                 [com.datomic/datomic-free "0.9.5206" :exclusions [joda-time]]]
   :main ^:skip-aot molecule.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
