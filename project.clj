9e9d1c9522beda7ca67a380bd6cf092a684b30d3
Peter Garbers <peter@garbers.me>
2017-04-12 13:26:23 -0400

    Move datomic to dev-dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject molecule "0.1.0-SNAPSHOT"
+(defproject molecule "0.1.1-SNAPSHOT"
   :description "A datomic wrapper to make queries easier"
   :url "https://www.github.com/petergarbers/molecule"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datomic/datomic-free "0.9.5206" :exclusions [joda-time]]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot molecule.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[com.datomic/datomic-free "0.9.5561" :exclusions [joda-time]]]}})
